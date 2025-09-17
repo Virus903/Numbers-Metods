da9f815ae7b1cd706d264f64b53044f4830c2fb6
Virus903 <kirillwsac2000@gmail.com>
2025-09-17 10:22:37 +0300

    Добавьте файлы проекта.

---
+++ Numbers Metods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Example
+{
+    private static int n;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива:");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Введите элемент массива № {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nЭлементы массива:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i]} ");
+        }
+
+        Console.WriteLine("\nвведите колличество первых элементов массива");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = numbers.Take(n).Sum();
+
+        Console.WriteLine($"Сумма первых {n} элементов: {sum}");
+    }
+}
